d57cc4b951b9c6c34626f7b982f22413d5925770
Scott Lease <scottie_lease@hotmail.com>
2021-09-14 14:20:09 -0700

    adding project files

---
+++ app.js
@@ -0,0 +1,19 @@
+let married = prompt("Am I married?");
+console.log(married);
+alert(null);
+
+let age = prompt("Am I over 35 years old?");
+console.log(age);
+alert(null);
+
+let live = prompt("Do I live in Seattle area?");
+console.log(live);
+alert(null);
+
+let car = prompt("Do I have a car?");
+console.log(car);
+alert(null);
+
+let cook = prompt("Do I enjoy cooking?");
+console.log(cook);
+alert(null);
